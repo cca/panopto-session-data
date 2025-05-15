1608bb54da7958a8ffb716c24d12559de2857c94
phette23 <phette23@gmail.com>
2025-05-15 12:19:33 -0700

    fix: if_exists=replace now not overwrite

--- insert.py
+++ insert.py
@@ -2,12 +2,12 @@ import sqlite3
 
 import pandas as pd
 
-conn = sqlite3.connect('pano.db')
+conn = sqlite3.connect("pano.db")
 # load the data into a Pandas DataFrame
-sessions = pd.read_csv('sessionscreatedoredited.csv', low_memory=False)
+sessions = pd.read_csv("sessionscreatedoredited.csv", low_memory=False)
 # write the data to a sqlite table
-sessions.to_sql('sessions', conn, if_exists='overwrite', index=False)
+sessions.to_sql("sessions", conn, if_exists="replace", index=False)
 
-usage = pd.read_csv('sessionusage.csv', low_memory=False)
-usage.to_sql('sessions', conn, if_exists='overwrite', index=False)
+usage = pd.read_csv("sessionusage.csv", low_memory=False)
+usage.to_sql("usage", conn, if_exists="replace", index=False)
 conn.close()
